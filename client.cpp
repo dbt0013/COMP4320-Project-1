3dbea88e072850c01f5a11fda7c854ef58cfb0e6
cwtillery <carsonwtillery@gmail.com>
2022-11-01 18:55:40 -0500

    Added gremlin function

--- client.cpp
+++ client.cpp
@@ -8,13 +8,36 @@
 
 using namespace std;
 
-void error(char *msg)
-{
-    perror(msg)
-    exit(EXIT_FAILURE)
+void error(char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
 }
 
-int main()
-{
+void gremlin(char packet[], int packetLength, double probability) {
+    double randValue = rand() / RAND_MAX;
+    if (randValue > probability) {
+        return;
+    }
+    
+    if (randValue >= 0 && randValue < 0.5) {
+        packet[(int)(randValue * packetLength)] = '0';
+    }
+    else if (randValue >= 0.5 && randValue < 0.8) {
+        for (int i = 0; i < 2; i++) {
+            packet[(int)(randValue * packetLength)] = '0';
+        }
+    } 
+    else {
+        for (int i = 0; i < 3; i++){
+            packet[(int)(randValue * packetLength)] = '0';
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        error("error: no IP");
+    }
+
     return 0;
 }
